367e0fe8b3d05fadb3e25761df664e7d038763c6
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2023-04-09 09:41:15 +0300

    Initial Commit

---
+++ ColorizedApp-SwiftUI/ColorizedApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_SwiftUIApp.swift
+//  ColorizedApp-SwiftUI
+//
+//  Created by Елена Логинова on 09.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
